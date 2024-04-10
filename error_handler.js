c86e8c7b1a0f07d8dd35118c64b51c541daa2d81
asGabriel <andrade.gabriel2295@gmail.com>
2024-04-10 06:43:53 -0300

    feat: clear input after insert a task

--- error_handler.js
+++
@@ -1,3 +0,0 @@
-export const showErrorMessage = (message) => {
-  alert(message);
-};
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import { createTask, renderListItemEvents, renderList } from "./handler.js";
 
-const input = document.getElementById("taskName");
+export const inputTaskHTML = document.getElementById("taskName");
 const button = document.getElementById("newTaskButton");
 
 const PageEventsListener = () => {
   button.addEventListener("click", () => {
-    createTask(input.value);
+    createTask(inputTaskHTML.value);
     renderListItemEvents();
   });
 };
